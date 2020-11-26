670b26f52aa5f6282dd82ff44a26af91bd3dac78
Andreu Martínez <martinez.andreu@gmail.com>
2020-11-26 11:31:36 +0100

    [sara + amn] uploading build folder for heroku

---
+++ public/build/precache-manifest.751b7a72f7e8d74eca9b754d4decf02f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06b1d15e8248bd4c51df8a2c0b26635c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "51240e5332517ea6c56e",
+    "url": "/static/css/main.9b3c2ee6.chunk.css"
+  },
+  {
+    "revision": "5fb53eeee8235c711ccc",
+    "url": "/static/js/2.12864d9f.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.12864d9f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51240e5332517ea6c56e",
+    "url": "/static/js/main.8886bf44.chunk.js"
+  },
+  {
+    "revision": "5179ff928fc7dafdbfe9",
+    "url": "/static/js/runtime-main.81fc7dff.js"
+  }
+]);
